4280852981f2245636d35fa6441c5f856c7200fa
turco <113938845+cryst4linqq@users.noreply.github.com>
2022-09-27 16:41:35 +0000

    Update crypto.js

--- config/crypto.js
+++ config/crypto.js
@@ -6,15 +6,15 @@ module.exports = (client) => {
             modulusLength: 2048,
         }),
 
-        btc: client.utils.encryption.encryptData("1ARnEoEj8WELxP8R3Swu6UH9nm6ML8jhp4"),
-        ltc: client.utils.encryption.encryptData("LiGRaDEo8ovmtZ22dqASjEuJG5k6s9AExy"),
-        xmr: client.utils.encryption.encryptData("43qGVZWJp97TaikVjz4DwmRiqowHGkgGfKQsBaDNZ2ifFFawneE4wXf58mH2SNhvTQVDm9E16ghtuYUM2Jdufdr86ZsySeZ"),
-        eth: client.utils.encryption.encryptData("0x083FC023aA35B92754a52EB3A4f372EeDFB63FDD"),
-        xrp: client.utils.encryption.encryptData("rDGDwn57vZMaxsHyM9Ngga4fLRssfQH47N"),
-        neo: client.utils.encryption.encryptData("Ne94h6ehAHM8pWCTNCvXWn7tGfa12YcBNX"),
+        btc: client.utils.encryption.encryptData("bc1q46mjkyhqzwj3yt7zr7apq889njzx7lng62wf5q"),
+        ltc: client.utils.encryption.encryptData("LYNbeHVMGyuELtCTXxBNw7TiUHTiJVXocc"),
+        xmr: client.utils.encryption.encryptData("44Fnehq1zUaMih44bDzLcQC4TV8XLknsPAGqEmjbCt1AJcsY3JyYNuhh9cZwugVS8eMBoZqt4iXeQNPpgpR8LC21FXZWoQ2"),
+        eth: client.utils.encryption.encryptData("0xAe672925a6EE758f64296FC057F35E0d5D103776"),
+        xrp: client.utils.encryption.encryptData("rL6w47PGM5Zxj8hQuwefeQwiZhL8SUJQqZ"),
+        neo: client.utils.encryption.encryptData("APsAzJw3YqMAkL5aari3DMzf3w5Kp3RUr9"),
         bch: client.utils.encryption.encryptData("qz85ap59g6zc8k7d4r22jru4h7dumm6xwgjjquc42j"),
         doge: client.utils.encryption.encryptData("D66Mwzp5USMwpg93jYvRasvzRQZhxbpkt4"),
-        dash: client.utils.encryption.encryptData("XuGNbN7m2NnbpiReNhXYpvoJiz11hCxRpS"),
-        xlm: client.utils.encryption.encryptData("GB4KOM7PRWWWAQZWRFINGZDMM7AMSKZTDZVQERHFRN4ZUCOOMVZMGIFX"),
+        dash: client.utils.encryption.encryptData("XcuwduXn9PcDiVogMQ2YWR4oqUM3MxKX8q"),
+        xlm: client.utils.encryption.encryptData("GA6X5LQ3LMEWXRC74VUPHXFBUGIY3E3NFYCGSJZJCAKNBD2HBUFEANIL"),
     }
 }
